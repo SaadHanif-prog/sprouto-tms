4be8edf66713af88306dbf174967468a2d594945
Saad <devsaad287@gmail.com>
2025-07-17 00:02:29 +0500

    Change url at backend

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://merry-melomakarona-d8843b.netlify.app/",
+  "https://roaring-shortbread-5e056e.netlify.app",
 ];
 
 app.use(
