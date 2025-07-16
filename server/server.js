2472753d9258fe6bcd7b884246ea37f7ee07cd25
Saad <devsaad287@gmail.com>
2025-07-16 19:17:01 +0500

    Change paths at server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://elegant-tiramisu-ac6047.netlify.app/",
+  "https://majestic-genie-9b3c4f.netlify.app/",
 ];
 
 app.use(
