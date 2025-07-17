4167171ef99a1dea7adb84b6655440faa1751050
Saad <devsaad287@gmail.com>
2025-07-17 18:41:05 +0500

    final fix

--- client/url.js
+++ client/url.js
@@ -1,20 +1,19 @@
-// const BASE_URL = "http://localhost:5000";
-const BASE_URL = "https://sprouto-tms.onrender.com";
+const BASE_URL = "/api/v1";
 
 const API_PATHS = {
   // Task routes
-  GET_TASKS: `${BASE_URL}/api/v1/tasks`,
-  CREATE_TASK: `${BASE_URL}/api/v1/tasks`,
-  UPDATE_TASK: (id) => `${BASE_URL}/api/v1/tasks/${id}`,
-  UPDATE_TASK_STATUS: (id) => `${BASE_URL}/api/v1/tasks/${id}/status`,
-  DELETE_TASK: (id) => `${BASE_URL}/api/v1/tasks/${id}`,
-  GET_TASK_BY_ID: (id) => `${BASE_URL}/api/v1/tasks/${id}`,
+  GET_TASKS: `${BASE_URL}/tasks`,
+  CREATE_TASK: `${BASE_URL}/tasks`,
+  UPDATE_TASK: (id) => `${BASE_URL}/tasks/${id}`,
+  UPDATE_TASK_STATUS: (id) => `${BASE_URL}/tasks/${id}/status`,
+  DELETE_TASK: (id) => `${BASE_URL}/tasks/${id}`,
+  GET_TASK_BY_ID: (id) => `${BASE_URL}/tasks/${id}`,
 
   // Auth routes
-  SIGNUP: `${BASE_URL}/api/v1/auth/signup`,
-  LOGIN: `${BASE_URL}/api/v1/auth/login`,
-  LOGOUT: `${BASE_URL}/api/v1/auth/logout`,
-  CHECK_AUTH: `${BASE_URL}/api/v1/auth/check-auth`,
+  SIGNUP: `${BASE_URL}/auth/signup`,
+  LOGIN: `${BASE_URL}/auth/login`,
+  LOGOUT: `${BASE_URL}/auth/logout`,
+  CHECK_AUTH: `${BASE_URL}/auth/check-auth`,
 };
 
 export default API_PATHS;
